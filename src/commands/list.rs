c5a7a0e0437253655a1d1557660296775f2e61aa
Sjoertjuh <63722509+Sjoertjuh@users.noreply.github.com>
2023-10-31 16:14:42 +0100

    chore: use comfy-table package for creating list table

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,6 +1,10 @@
 use async_trait::async_trait;
 use log::info;
 
+use comfy_table::{Table, ContentArrangement};
+use comfy_table::presets::UTF8_FULL;
+use comfy_table::modifiers::UTF8_ROUND_CORNERS;
+
 use crate::{cli::{ListArgs, HasAccessToken}, api::{WordsmithClient, WordsmithError}};
 
 use super::Execute;
@@ -15,31 +19,23 @@ impl Execute for ListArgs {
 
         info!("Found {} projects for \"{}\"", company.projects.len(), company.name);
 
-        let id_width = company.projects.iter().map(|p| p.id.to_string().len()).max().unwrap_or(2).max(2);
-        let name_width = company.projects.iter().map(|p| p.name.len()).max().unwrap_or(4);
+        let mut table = Table::new();
+        table
+            .load_preset(UTF8_FULL)
+            .apply_modifier(UTF8_ROUND_CORNERS)
+            .set_content_arrangement(ContentArrangement::Dynamic)
+            .set_header(vec!["ID", "Name", "Default lang", "Description"]);
 
-        let term_width = term_size::dimensions().map(|(w, _)| w).unwrap_or(80);
-
-        println!(
-            "{:id_width$}   {:name_width$}  {:default_lang_width$}  {}",
-            "ID", "Name", "Default lang", "Description",
-            id_width = id_width, name_width = name_width, default_lang_width = 12
-        );
         for project in company.projects {
-            let description = project.description.unwrap_or("-".to_string());
-            let truncated_description = if description.len() > term_width - id_width - name_width - 12 - 6 {
-                let mut truncated_description = description[..term_width - id_width - name_width - 12 - 6 - 4].to_string();
-                truncated_description.push_str("...");
-                truncated_description
-            } else {
-                description.to_string()
-            };
-            println!(
-                "{:id_width$}.  {:name_width$}  {:default_lang_width$}  {}",
-                project.id, project.name, project.default_language, truncated_description,
-                id_width = id_width, name_width = name_width, default_lang_width = 12
-            );
+            table.add_row(vec![
+                project.id.to_string(),
+                project.name,
+                project.default_language,
+                project.description.unwrap_or_default(),
+            ]);
         }
+
+        println!("{table}");
 
         Ok(())
     }
